1001150710adf9112499379b9d070702296f79f7
Deniss Fjodorovs <3starblaze@gmail.com>
2020-07-28 10:21:41 +0300

    Autolint code

--- src/App/script.js
+++ src/App/script.js
@@ -3,24 +3,24 @@ import SettingsComponent from '../SettingsComponent/index.vue';
 import ControlBarComponent from '../ControlBarComponent/index.vue';
 
 export default {
-    name: 'App',
-    components: {
-        "settings": SettingsComponent,
-        "control-bar": ControlBarComponent,
+  name: 'App',
+  components: {
+    settings: SettingsComponent,
+    'control-bar': ControlBarComponent,
+  },
+  created() {
+    bus.$on('askNotification', () => {
+      Notification.requestPermission().then((result) => {
+        this.notificationPermission = result;
+      });
+    });
+  },
+  data: () => ({
+    notificationPermission: Notification.permission,
+  }),
+  watch: {
+    notificationPermission(newPermission) {
+      bus.$emit('notificationChange', newPermission);
     },
-    created() {
-        bus.$on('askNotification', () => {
-            Notification.requestPermission().then(result => {
-                this.notificationPermission = result;
-            });
-        });
-    },
-    data: () => ({
-        notificationPermission: Notification.permission,
-    }),
-    watch: {
-        notificationPermission: function(newPermission) {
-            bus.$emit('notificationChange', newPermission);
-        }
-    },
-}
+  },
+};
