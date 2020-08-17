178c21c3159962ff05e118aa08fe8ee54909cbd3
Deniss Fjodorovs <3starblaze@gmail.com>
2020-08-17 11:22:54 +0300

    Notify that notifications are enabled in settings

--- src/SettingsComponent/index.vue
+++ src/SettingsComponent/index.vue
@@ -6,6 +6,7 @@
                 Notifications are not enabled for this site.
                 <button @click="askNotification">Enable notifications</button>
             </li>
+            <li v-else>Notifications are enabled.</li>
             <li>
                 Pure view shortcut: <b>{{ globalData.pureViewShortcut }}</b>
             </li>
