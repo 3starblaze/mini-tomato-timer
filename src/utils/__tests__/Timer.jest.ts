b0fa82a917fdca6bac23e3c6a3b9511a0a5bef64
Deniss Fjodorovs <3starblaze@gmail.com>
2020-08-27 10:02:41 +0300

    Fix tests

--- src/utils/__tests__/Timer.jest.ts
+++ src/utils/__tests__/Timer.jest.ts
@@ -1,16 +1,13 @@
 import Timer from '../Timer.ts';
 
 test('Expect positive time to work fine', () => {
-  const timer = new Timer();
-  expect(() => { timer.currentTime = -2000; }).not.toThrow();
+  expect(() => { new Timer(3000); }).not.toThrow();
 });
 
 test('Expect 0 time to work fine', () => {
-  const timer = new Timer();
-  expect(() => { timer.currentTime = 0; }).not.toThrow();
+  expect(() => { new Timer(0); }).not.toThrow();
 });
 
 test('Expect negative time to throw', () => {
-  const timer = new Timer();
-  expect(() => { timer.currentTime = -2000; }).toThrow();
+  expect(() => { new Timer(-2000); }).toThrow();
 });
